58cea09b01d24c72154ed46a0d8e9ecb2759fe59
Leonardo <odriozola-leo@hotmail.com>
2022-02-22 19:53:06 -0300

    proyectos backend y frontend en blancos

---
+++ demo/src/main/java/uy/com/sofka/demo/TodoRepository.java
@@ -0,0 +1,7 @@
+package uy.com.sofka.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TodoRepository extends CrudRepository <Todo, Long>{
+
+}
---
+++ demo/src/test/java/uy/com/sofka/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.com.sofka.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
